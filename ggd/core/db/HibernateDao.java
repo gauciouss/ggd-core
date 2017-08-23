d24d2dff412edd70b304796b62808a77bf2e28d5
gauciouss <gauciouss@gmail.com>
2017-08-24 00:45:09 +0800

    重新定義核心組件

--- ggd/core/db/HibernateDao.java
+++ ggd/core/db/HibernateDao.java
@@ -1,5 +1,23 @@
 package ggd.core.db;
 
-public class HibernateDao {
+import java.io.Serializable;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import baytony.db.hibernate.BasicDao;
+
+public class HibernateDao<Bean extends Serializable, ID extends Serializable> extends BasicDao<Bean, ID> {
+
+	
+	@Autowired(required = true)
+	@Qualifier("SessionFactory")
+	private SessionFactory sessionFactory;
+	
+	@Override
+	protected Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
 }
